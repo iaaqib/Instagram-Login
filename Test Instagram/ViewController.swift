b1f926fb0a46e44a4995316d38a676487265e308
Aaqib Hussain <aqib.hussain@invisionsolutions.ca>
2017-08-02 19:11:30 +0500

    Initial Commit

---
+++ Test Instagram/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Test Instagram
+//
+//  Created by Invision Solutions on 02/08/2017.
+//  Copyright © 2017 Invision Solutions. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
